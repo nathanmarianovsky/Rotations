ce080409e2c22371816f05f166e9ac439c084ef8
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2015-12-17 05:26:10 -0500

    Cleaned Up and Fully Functional
    Rotation by quaternions and rotation matrix both work.

--- JS Implementation/Quaternions.js
+++ JS Implementation/Quaternions.js
@@ -27,10 +27,6 @@ exports.quaternion_multiplication = function(p,q) {
 		p_vector = p.splice(1),
 		q_vector = q.splice(1); 
 	product.push((p_knot * q_knot) - exports.dot_product(p_vector,q_vector));
-	// for(var i = 0; i < p_vector.length; i++) {
-	// 	p_vector[i] *= q_knot;
-	// 	q_vector[i] *= p_knot;
-	// }
 	var cross = exports.cross_product(p_vector,q_vector),
 		vector = [];
 	for(var i = 0; i < p_vector.length; i++) {
@@ -57,33 +53,24 @@ exports.normalize = function(obj) {
 
 // Performs the quaternion rotation mapping which returns the rotated vector
 exports.mapping = function(obj) {
-	var angle = obj["alpha"] * (Math.PI / 180);
-	var vec = [];
-	// console.log(obj);
+	var angle = obj["alpha"] * (Math.PI / 180),
+		vec = [];
 	vec.push(0);
 	for(var i = 0; i < obj["vector"].length; i++) {
 		vec.push(obj["vector"][i]);
-		// console.log(vec);
 	}
 	var quaternion = [
 			Math.cos(angle / 2), 
 			obj["axis"][0] * Math.sin(angle / 2),
 			obj["axis"][1] * Math.sin(angle / 2),
 			obj["axis"][2] * Math.sin(angle / 2)
-		];
-	var	quaternion_inverse = [
+		],
+		quaternion_inverse = [
 			Math.cos(angle / 2), 
 			-obj["axis"][0] * Math.sin(angle / 2),
 			-obj["axis"][1] * Math.sin(angle / 2),
 			-obj["axis"][2] * Math.sin(angle / 2)
 		];
-	// console.log(quaternion);
-	// console.log(vec);
 	var	first_product = exports.quaternion_multiplication(quaternion, vec);
-		// console.log(quaternion);
-		// console.log(vec);
-		console.log(first_product, quaternion_inverse);
-	// console.log(obj);
-	// console.log(first_product);
 	return exports.quaternion_multiplication(first_product, quaternion_inverse);
 };
