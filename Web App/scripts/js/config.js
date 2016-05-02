b14f4da20695f70216cb4790239ebc0dd855132b
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2016-05-01 21:02:15 -0400

    Working App!

--- Web App/scripts/js/config.js
+++ Web App/scripts/js/config.js
@@ -6,6 +6,7 @@ require.config({
         jquery: "/node_modules/jquery/dist/jquery.min",
         materialize: "/bower_components/materializecss-amd/dist/materialize.amd.min",
         three: "/scripts/three.js/build/three.min",
+        router5: "/node_modules/router5/dist/amd/router5.min",
         mathjax: "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
     },
     shim: {
@@ -24,13 +25,6 @@ require.config({
             }
         }
     }
-});
-
-requirejs(["/node_modules/masonry-layout/dist/masonry.pkgd.js",], Masonry => {
-    new Masonry( '.grid', {
-        columnWidth: ".grid-sizer",
-        itemSelector: ".grid-item"
-    });
 });
 
 require(["app/main"]);
--- Web App/scripts/js/main.js
+++ Web App/scripts/js/main.js
@@ -1,47 +1,79 @@
-define(["jquery", "materialize", "mathjax", "three"], ($, Materialize, MathJax, THREE) => {
+define(["jquery", "materialize", "router5", "mathjax", "app/links", "three"], ($, Materialize, router5, MathJax, links, THREE) => {
 	$(function() {
 
-		var check_input = input => { return Number(input) == input && input !== ""; };
+		// $('select').material_select();
 
-		$('select').material_select();
+		// import listenersPlugin from 'router5-listeners';
 
-		$("#click").click(event => {
-			event.preventDefault();
-			var arr = ["angle-value", "axis-x", "axis-y", "axis-z", "vector-x", "vector-y", "vector-z"];
-			if(arr.some(elem => check_input($("#" + elem).val()) !== true)) { alert("Either you have not provided all of the necessary information or one of the values is not a number!"); }
-			else {
-				var method = "",
-					angle = "",
-					axis = "",
-					vector = "",
-					text = "",
-					gridItem = $("<div>").addClass("grid-item"),
-					row1 = $("<div>").addClass("row"),
-					form = $("<form>").addClass("col s12"),
-					row2 = $("<div>").addClass("row"),
-					title = $("<h4>").addClass("center").text("Rotated Vector"),
-					inputField = $("<div>").addClass("input-field col s12 center");
-				$("#method").val() == 1 ? method = "quaternion" : method = "matrix";
-				$("#angle-type").val() == 1 ? angle = $("#angle-value").val() : angle = Number($("#angle-value").val()) * (180 / Math.PI);
-				axis = $("#axis-x").val() + "-" + $("#axis-y").val() + "-" + $("#axis-z").val();
-				vector = $("#vector-x").val() + "-" + $("#vector-y").val() + "-" + $("#vector-z").val();
+		var router = new router5.Router5([
+			new router5.RouteNode("def", "/"),
+			new router5.RouteNode("method", "/rotation/:method", [
+				new router5.RouteNode("angle", "/:angle", [
+					new router5.RouteNode("axis", "/:axis", [
+						new router5.RouteNode("output", "/:vector")
+					])
+				])
+			])
+		],{
+			defaultRoute: "def"
+		});
+
+		router.addRouteListener("def", (toState, fromState) => {
+			$.get("/client/input.html").done(data => {
+				$("body").empty();
+				$("body").append(data);
+				$('select').material_select();
+				requirejs(["/node_modules/masonry-layout/dist/masonry.pkgd.js",], Masonry => {
+				    new Masonry( '.grid', {
+				        columnWidth: ".grid-sizer",
+				        itemSelector: ".grid-item"
+				    });
+				});
+				links.add_links(router);
+			});
+		});
+
+		router.addRouteListener("method.angle.axis.output", (toState, fromState) => {
+			var angle = toState.params.angle,
+				axisArr = toState.params.axis.split("-"),
+				vectorArr = toState.params.vector.split("-"),
+				method = toState.params.method,
+				axis = toState.params.axis,
+				vector = toState.params.vector,
+				text = [];
+			$.get("/client/output.html").done(data => {
+				$("body").empty();
+				$("body").append(data);
 				$.get("/" + method + "/" + angle + "/" + axis + "/" + vector).done(data => {
-					text = "You provided the axis, $\\eqalign{\\begin{pmatrix} " + $("#axis-x").val() + " \\\\ " + $("#axis-y").val() + " \\\\ " + $("#axis-z").val() + " \\end{pmatrix}}$, wanting to rotate the vector, $\\eqalign{\\begin{pmatrix} " + $("#vector-x").val() + " \\\\ " + $("#vector-y").val() + " \\\\ " + $("#vector-z").val() + " \\end{pmatrix}}$, by " + angle + " degrees. With the use of the " + method + " method, the result is, $\\eqalign{\\begin{pmatrix} " + data[0] + " \\\\ " + data[1] + " \\\\ " + data[2] + " \\end{pmatrix}}$.";
-					inputField.text(text);
-					row2.append(title).append(inputField);
-					form.append(row2);
-					row1.append(form);
-					gridItem.append(row1);
-					$(".grid").append(gridItem);
+					// text.push("$\\eqalign{\\begin{pmatrix} " + Number($("#axis-x").val()).toFixed(6) + " \\\\ " + Number($("#axis-y").val()).toFixed(6) + " \\\\ " + Number($("#axis-z").val()).toFixed(6) + " \\end{pmatrix}}$");
+					// text.push("$\\eqalign{\\begin{pmatrix} " + Number($("#vector-x").val()).toFixed(6) + " \\\\ " + Number($("#vector-y").val()).toFixed(6) + " \\\\ " + Number($("#vector-z").val()).toFixed(6) + " \\end{pmatrix}}$");
+					// text.push("$" + angle + "^\\circ$");
+					// method == "quaternion" ? text.push("Quaternions") : text.push("Rotation Matrix");
+					// text.push("$\\eqalign{\\begin{pmatrix} " + data[0] + " \\\\ " + data[1] + " \\\\ " + data[2] + " \\end{pmatrix}}$");
+					// console.log(axis,vector,angle,data);
+					// for(var i = 0; i < text.length; i++) {
+					// 	$("#" + arr[i] + " .input-field").text(text[i]);
+					// }
+					$("#axis-form .input-field").append($("<div>").addClass("center").text("$\\eqalign{\\begin{pmatrix} " + Number(axisArr[0]).toFixed(6) + " \\\\ " + Number(axisArr[1]).toFixed(6) + " \\\\ " + Number(axisArr[2]).toFixed(6) + " \\end{pmatrix}}$"));
+					$("#vector-form .input-field").append($("<div>").addClass("center").text("$\\eqalign{\\begin{pmatrix} " + Number(vectorArr[0]).toFixed(6) + " \\\\ " + Number(vectorArr[1]).toFixed(6) + " \\\\ " + Number(vectorArr[2]).toFixed(6) + " \\end{pmatrix}}$"));
+					$("#angle-form .input-field").append($("<div>").addClass("center").text("$" + Number(angle).toFixed(6) + "^\\circ$"));
+					method == "quaternion" ? $("#method-form .input-field").append($("<div>").addClass("center").text("Quaternions")) : $("#method-form .input-field").append($("<div>").addClass("center").text("Rotation Matrix"));
+					$("#result-form .input-field").append($("<div>").addClass("center").text("$\\eqalign{\\begin{pmatrix} " + data[0] + " \\\\ " + data[1] + " \\\\ " + data[2] + " \\end{pmatrix}}$"));
+					// $("#info").append(grid);
 					requirejs(["/node_modules/masonry-layout/dist/masonry.pkgd.js",], Masonry => {
 					    new Masonry( '.grid', {
 					        columnWidth: ".grid-sizer",
 					        itemSelector: ".grid-item"
 					    });
 					});
+					links.add_links(router);
 					MathJax.Hub.Queue(["Typeset",MathJax.Hub,"main"]);
 				});
-			}
+			});
 		});
+
+		router.start();
+
+		
 	});
 });
